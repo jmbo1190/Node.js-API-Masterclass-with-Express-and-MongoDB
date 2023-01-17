428a256b8e432a7afaaa39f23451a63b3b55094d
Jean-Michel Bodart <jmbodart@skynet.be>
2023-01-17 15:14:00 +0100

    update server.js
    structured handling of methods and routes

--- __VIDEOS/NODEHTTP/server.js
+++ __VIDEOS/NODEHTTP/server.js
@@ -27,98 +27,57 @@ const todos = [
 
 const server = http.createServer((req, res) => {
     const { headers, url, method } = req;
-    console.log(headers, url, method);
+    console.log(method, url);
 
-    let ok = true;  // change the type of response
-    let ctnt;
-    if (["/html", "/text", "/json"].indexOf(url) !== -1) { 
-        ctnt = url.slice(1);
-        ok = true;
-    } else {
-        ok = false;
-        ctnt = "json";
-    }
-    if (ok) {
-        // Return default status 200 - OK
-        res.setHeader('X-Powered-By', 'Node.js');
-        if (ctnt === "html") {
-            res.setHeader('Content-Type', 'text/html'); // make the browser render html tags
-            res.write("<h1>Hello</h1>");
-            res.write("<h2>Hello Again</h2>");
-            res.end();  // send a response to avoid client hanging 
+    let body = [];
 
-        } else if (ctnt === "text") {
-            res.setHeader('Content-Type', 'text/plain'); // make the browser display html tags as text
-            res.write("<h1>Hello</h1>");
-            res.write("<h2>Hello Again</h2>");
-            res.end();  // send a response to avoid client hanging 
+    req.on("data", (chunk) => {
+        body.push(chunk);
+    }).on("end", () => {
+        body = Buffer.concat(body).toString();
+        //console.log("body:\n", body);
 
-        } else if (ctnt === "json") {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                success: true,
-                data: todos, 
-            }));  // send a response to avoid client hanging     
+        let status = 404;
+        const response = {
+            success: false,
+            data: null,
         }
 
-    } else {
-        if (url === "/protected"){
-            // Return 401 Status - Unauthorized
-            res.writeHead(401, {
-                'X-Powered-By': 'Node.js',
-                'Content-Type': 'application/json',
-            });
-            res.end(JSON.stringify({
-                success: false,
-                error: "Not authorized",
-                data: null, 
-            }));  // send a response to avoid client hanging 
 
-        } else if (url.match(/^\/register/)){
-            if (url.match(/^\/register\?(.+\&)?email=\w+@(\w+\.)+\w+/)) {
-                // Return 204 - Successfull, no content
-                res.writeHead(204, {
-                    'X-Powered-By': 'Node.js',
-                    'Content-Type': 'application/json',
-                });
-                res.end();
-
-            } else {
-                // Return 400 Status - Bad request
-                res.writeHead(400, {
-                    'X-Powered-By': 'Node.js',
-                    'Content-Type': 'application/json',
-                });
-                res.end(JSON.stringify({
-                    success: false,
-                    error: "Please provide valid e-mail",
-                    data: null, 
-                }));  // send a response to avoid client hanging 
-
+        if (method === "GET" && url === "/todos") {
+            status = 200;
+            response.success = true;
+            response.data = todos;
+        } else if (method === "POST" && url === "/todos"){
+            try{
+                const { id, text } = JSON.parse(body);
+                if (!id || !text) {
+                    status = 400; // bad request
+                    response.error = "Please specify both id and text";
+                    console.log("response:\n", response);
+                } else {
+                    todos.push({ id, text });
+                    status = 201; // created
+                    response.success = true;
+                    response.data = todos;
+                }
+            } catch(e){
+                status = 400;
+                response.error = e.message;
+                console.log("response:\n", response);
             }
-        } else {
-            // Return 404 Status - resource not found
-            /*
-            // basic way to set headers
-            res.statusCode = 404;
-            res.setHeader('X-Powered-By', 'Node.js');
-            res.setHeader('Content-Type', 'application/json');
-            */
-            // alternative to the above
-            res.writeHead(404, {
-                'X-Powered-By': 'Node.js',
-                'Content-Type': 'application/json',
-            });
-            res.end(JSON.stringify({
-                success: false,
-                error: "Not found",
-                data: null, 
-            }));  // send a response to avoid client hanging 
-
+            
+            
         }
-        
-    }
-    
+
+        res.writeHead(status, {
+            'X-Powered-By': 'Node.js',
+            'Content-Type': 'application/json',
+        });
+
+        res.end(JSON.stringify(response));  // send a response to avoid client hanging 
+
+    })
     
 });
 
